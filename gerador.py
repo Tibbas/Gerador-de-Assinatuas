8dd52fe591d18354216381540ae4dbc920966cf9
Rafael Resende <166184211+Tibbas@users.noreply.github.com>
2026-01-15 17:04:19 -0300

    Update gerador.py

--- gerador.py
+++ gerador.py
@@ -76,7 +76,7 @@ def tela_login():
 def tela_gerador():
     st.title("📚 Gerador de Assinaturas")
 
-    col1, col2 = st.columns([80, 1])
+    col1, col2 = st.columns([80, 60])
     with col2:
         if st.button("🚪 Sair"):
             st.session_state.pagina = "login"
@@ -290,5 +290,6 @@ if st.session_state.pagina == "login":
 
 elif st.session_state.pagina == "gerador":
     tela_gerador()
+
 
 
